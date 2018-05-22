6bdc0e94424e30e7911e5a598dc7f2a30ffbb6ba
Luke <lhahn16@student.sl.onca>
2018-05-21 22:32:47 -0400

    removed test values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import {FormsModule} from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import {AuthService} from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +11,6 @@ import { RegisterComponent } from './Register/Register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent,
     NavComponent,
     HomeComponent,
     RegisterComponent
